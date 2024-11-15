8ac2d423297f5c3aa329e08f29577f031d9b6d92
berez <berez@STAROSTA>
2024-11-15 14:39:27 +0200

    131

---
+++ DataBase/ConnectionClass.cs
@@ -0,0 +1,39 @@
+﻿using MySqlConnector;
+using System.IO;
+
+namespace DataBase
+{ 
+    class ConnectionClass
+    {
+        // Замість абсолютного шляху, можна комбінувати з поточною директорією
+        static string filePath1 = Path.Combine(Directory.GetCurrentDirectory(), @"..\..\..\DataBase\appsettings.json");
+
+        // Очищаємо шлях (щоб вийти з каталогу bin\Debug або bin\Release)
+        static string filePath = Path.GetFullPath(filePath1);  // Отримаємо абсолютний шлях
+
+        static string connStr = ConfigurationManager.LoadAppSettings(filePath).connectionStrings.DefaultConnection;
+
+        MySqlConnection sqlConn = new MySqlConnection(connStr);
+
+        public void openConnection()
+        {
+            if (sqlConn.State == System.Data.ConnectionState.Closed)
+            {
+                sqlConn.Open();
+            }
+        }
+
+        public void closeConnection()
+        {
+            if (sqlConn.State == System.Data.ConnectionState.Open)
+            {
+                sqlConn.Close();
+            }
+        }
+
+        public MySqlConnection getConnection()
+        {
+            return sqlConn;
+        }
+    }
+}
