224ce449ec847215561fb0dccd2d71bca26719fb
AndrijShwed <a_shwed@ukr.net>
2026-03-25 15:53:15 +0200

    54654

--- DataBase/Населені_Пункти.cs
+++ DataBase/Населені_Пункти.cs
@@ -115,110 +115,6 @@ namespace DataBase
             Program.OpenForm(this, form);
         }
 
-        //private void buttonОновити_Click(object sender, EventArgs e)
-        //{
-        //    dataGridViewНаселені_Пункти.DataSource = null;
-        //    dataGridViewНаселені_Пункти.Rows.Clear();
-        //    _data.Clear();
-
-        //    //user = new User();
-
-        //    ConnectionClass _manager = new ConnectionClass();
-
-        //    _manager.openConnection();
-
-        //    List<string> count_v = new List<string>();
-        //    this.dataGridViewНаселені_Пункти.Rows.Add();
-
-        //    for (int i = 0; i < dataVillage.Count; i++)
-        //    {
-        //        string count1 = @"
-        //            SELECT COUNT(*) 
-        //            FROM people p
-        //            JOIN villagestreet vs ON p.villagestreetId = vs.id
-        //            JOIN villages v ON vs.villageId = v.id
-        //            WHERE v.name = @name AND registr = 'так'";
-        //        count_v.Add(count1);
-        //    }
-
-
-        //    int all = 0;
-        //    int sum;
-        //    List<int> sum_v = new List<int>();
-        //    for (int i = 0; i < dataVillage.Count; i++)
-        //    {
-        //        MySqlCommand search = new MySqlCommand(count_v[i], _manager.getConnection());
-        //        search.Parameters.AddWithValue("@name", dataVillage[i].Name);
-
-        //        object result = search.ExecuteScalar();
-        //        sum = (result == null) ? 0 : Convert.ToInt32(result);
-
-        //        all += sum;
-        //        sum_v.Add(sum);
-        //    }
-
-        //    dataGridViewНаселені_Пункти.Rows[0].Cells[0].Value = Convert.ToInt32(DateTime.Now.Year.ToString());
-        //    for (int i = 0; i < dataVillage.Count; i++)
-        //    {
-        //        dataGridViewНаселені_Пункти.Rows[0].Cells[i + 1].Value = sum_v[i];
-        //    }
-
-        //    dataGridViewНаселені_Пункти.Rows[0].Cells[dataVillage.Count + 1].Value = all;
-
-        //}
-
-        //private void buttonОновити_Click(object sender, EventArgs e)
-        //{
-        //    dataGridViewНаселені_Пункти.DataSource = null;
-        //    dataGridViewНаселені_Пункти.Rows.Clear();
-        //    _data.Clear();
-
-        //    ConnectionClass _manager = new ConnectionClass();
-        //    _manager.openConnection();
-
-        //    // ОДИН SQL ЗАПИТ ЗАМІСТЬ ЦИКЛУ
-        //    string query = @"
-        //                SELECT 
-        //                    v.name,
-        //                    COUNT(p.people_id) AS cnt
-        //                FROM villages v
-        //                LEFT JOIN villagestreet vs ON vs.villageId = v.id
-        //                LEFT JOIN people p 
-        //                    ON p.villagestreetId = vs.id 
-        //                    AND p.registr = 'так'
-        //                GROUP BY v.id, v.name";
-
-        //    MySqlCommand cmd = new MySqlCommand(query, _manager.getConnection());
-        //    MySqlDataReader reader = cmd.ExecuteReader();
-
-        //    int all = 0;
-
-        //    // додаємо один рядок у грід
-        //    dataGridViewНаселені_Пункти.Rows.Add();
-
-        //    int col = 1; // 0 колонка = рік
-
-        //    while (reader.Read())
-        //    {
-        //        int count = Convert.ToInt32(reader["cnt"]);
-        //        string name = reader["name"].ToString();
-
-        //        all += count;
-
-        //        dataGridViewНаселені_Пункти.Rows[0].Cells[col].Value = count;
-        //        col++;
-        //    }
-
-        //    reader.Close();
-        //    _manager.closeConnection();
-
-        //    // рік
-        //    dataGridViewНаселені_Пункти.Rows[0].Cells[0].Value = DateTime.Now.Year;
-
-        //    // загальна сума
-        //    dataGridViewНаселені_Пункти.Rows[0].Cells[col].Value = all;
-        //}
-
         private void buttonОновити_Click(object sender, EventArgs e)
         {
             dataGridViewНаселені_Пункти.DataSource = null;
@@ -300,9 +196,8 @@ namespace DataBase
             // 🧱 3. БУДУЄМО ТАБЛИЦЮ
             // =========================
 
-            dataGridViewНаселені_Пункти.Rows.Add();
 
-            int col = 1;
+            int col = 0;
 
             // 📌 додаємо snapshot роки
             foreach (var year in data.Keys.OrderBy(x => x))
