0fa78b9c1ea8871df358a3ff53aaf84b994cce48
berez <berez@STAROSTA>
2026-05-14 13:00:02 +0300

    3135

--- DataBase/Repositories/EnterpriseRepository.cs
+++ DataBase/Repositories/EnterpriseRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySqlConnector;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,32 @@ namespace DataBase.Repositories
 {
     public class EnterpriseRepository
     {
+        public Enterprise GetById(int id)
+        {
+            Enterprise row = null;
+            ConnectionClass conn = new ConnectionClass();
+            MySqlDataReader _reader;
+            conn.openConnection();
+            string sql = "SELECT e.id, e.name, v.name AS village, s.name AS street," +
+                        " e.number, e.head, e.employeesCount" +
+                        " FROM enterprise e" +
+                        " JOIN villagestreet vs ON p.villagestreetId = vs.id" +
+                        " JOIN villages v ON vs.villageId = v.id" +
+                        " JOIN streets s ON vs.streetId = s.id" +
+                        " WHERE e.id = @id";
 
+            MySqlCommand cmd = new MySqlCommand(sql, conn.getConnection());
+            cmd.Parameters.AddWithValue("@id", id);
+            _reader = cmd.ExecuteReader();
+
+            if (_reader.Read())
+            {
+                row = Enterprise.ReadOne(_reader);
+            }
+            _reader.Close();
+            conn.closeConnection();
+            return row;
+
+        }
     }
 }
